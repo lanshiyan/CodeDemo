b152c79fc4d82c5b7c86fa60cd5843cc1a4a6deb
亚林 單 <1304679383@qq.com>
2017-05-12 17:36:44 +0800

    webapi

---
+++ Webapi/Models/TestModels.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Webapi.Models
+{
+    public class TestModels
+    {
+        public int id { get; set; }
+
+        public string Name { get; set; }
+
+        public bool Gender { get; set; }
+
+        public DateTime BirthDay { get; set; }
+    }
+}
